faf2e08016f1e51c66eba61af988d4ef98443dd0
Tatiana Santiago <tatiana.santiago@stkbancomerans.com>
2018-06-29 17:58:42 -0500

    Softtek NTSM 29jun2018, Proyecto Progress Bar Web Components

--- tatis/Excercises/Divisa.js
+++ tatis/Excercises/Divisa.js
@@ -1,7 +1,3 @@
-//
-// const dolar = 19.85 ;
-// const rupia = 0.295 ;
-// const euro = 19.64 ;
 
 class Divisa {
 
@@ -18,32 +14,19 @@ class Divisa {
       return this.monto / this.divisa;
  }
 
-
- // calculoDolares(){
- //   return this.monto * dolar;
- // }
- //
- // calculoRupias(){
- //   return this.monto * rupia;
- // }
- //
- // calculoEuros(){
- //   return this.monto * euro;
- // }
-
 }
 
-const compra1 = new Divisa(1, 19.85); //USD
-console.log('conversion Pesos a Dólares: ' + compra1.conversion);
+const compra1 = new Divisa(10, 19.85); //USD
+console.log('conversion Dólares a Pesos : ' + compra1.conversion);
 
 
 const compra2 = new Divisa(1, 0.295); //INR
-console.log('conversion Pesos a Rupias: ' + compra2.conversion);
+console.log('conversion Rupias a Pesos: ' + compra2.conversion);
 
 
 const compra3 = new Divisa(1, 19.64);  //EUR
-console.log('conversion Pesos a Euros: ' + compra3.conversion);
+console.log('conversion Euros a Pesos: ' + compra3.conversion);
 
 
 const compra4 = new Divisa(500, 19.85); //USD
-console.log('conversion Pesos a Dólares: ' + compra1.conversionDesdePesos);
+console.log('Conversion Pesos a Dolares: ' + compra1.conversionDesdePesos);
